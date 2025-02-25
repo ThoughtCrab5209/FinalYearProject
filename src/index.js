c65dda42e540681d3bc50762e7fb548ae8057c5c
thoughtcrab5209 <avq.uca@protonmail.com>
2025-02-25 13:34:31 +0000

    Update: add basic page routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./pages/Layout.js";
+import Courses from "./pages/Courses.js";
+
+
+export default function App() {
+  const [windowSize, setWindowSize] = useState(1000);
+  
+  useEffect(() => {
+    function resizeWindow() {
+      setWindowSize(window.innerWidth)
+    }
+
+    window.addEventListener('resize', resizeWindow)
+  })
+
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="courses" element={<Courses/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
