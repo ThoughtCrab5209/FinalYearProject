b91798eec27ac67797736843a01b597db2d54f35
thoughtcrab5209 <avq.uca@protonmail.com>
2025-02-25 06:16:33 +0000

    Update: shift from custom components to MUI Card components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './styles.css';
-import CourseBox from './course-box';
 import React, { useEffect, useState } from 'react';
-import { IconButton } from '@mui/material';
+import { CardActionArea, IconButton } from '@mui/material';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
 
 
 function App() {
@@ -33,51 +35,285 @@ function App() {
           <hr/>
         </div>
 
-        <div style={{float: "left", width: "22%"}}>
-          <CourseBox
-            title={"CM3202 Emerging Technologies"} 
-            lecturers={"Carolina Fuentes Toro, Nedjma Ousidhoum"}
-          />
-        </div>
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>          
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>              
+              CM3202 Emerging Technologies
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Carolina Fuentes Toro, Nedjma Ousidhoum
+            </CardContent>
+          </CardActionArea>
+        </Card>
 
-        <div style={{float: "left", width: "22%"}}>
-          <CourseBox 
-            title={"CM3203 One Semester Individual Project - 40"}
-            lecturers={"Frank Langbein"}
-          />
-        </div>
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>          
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>              
+              CM3203 One Semester Individual Project - 40
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Frank Langbein
+            </CardContent>
+          </CardActionArea>
+        </Card>
 
-        <div style={{float: "left", width: "22%"}}>
-          <CourseBox
-            title={"CM3104 Large-Scale Databases"}
-            lecturers={"Alia Abdelmoty, Christopher Jones"}
-          />        
-        </div>
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>          
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>              
+              CM3104 Large-Scale Databases
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Alia Abdelmoty, Christopher Jones
+            </CardContent>
+          </CardActionArea>
+        </Card>
 
-        <div style={{float: "left", width: "22%"}}>
-          <CourseBox 
-            title={"CM3109 Combinatorial Optimisation"}
-            lecturers={"Richard Booth"}
-          />
-        </div>
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}> 
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM3109 Combinatorial Optimisation
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Richard Booth
+            </CardContent>
+          </CardActionArea>
+        </Card>
 
-        <div style={{float: "left", width: "22%"}}>
-          <CourseBox
-            title={"CM3110 Security"}
-            lecturers={"George Theodorakopoulos"}
-          />
-        </div>
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>          
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM3110 Security
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              George Theodorakopoulos
+            </CardContent>
+          </CardActionArea>
+        </Card>
 
-        <div style={{float: "left", width: "22%"}}>
-          <CourseBox
-            title={"CM3117 Political Economies of Computing"}
-            lecturers={"Sandy Gould"}
-          />
-        </div>
-
-
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM3117 Political Economies of Computing
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Sandy Gould
+            </CardContent>
+          </CardActionArea>
+        </Card>
       </div>
-        
+
+
+      <div className="App-Content">
+        <div className='Year-Title'>
+          Academic Year 23-24
+          <hr/>
+        </div>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM2500 Placement
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Martin Caminada, Joanna Emery, Leigh Hodge, James Osborne
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>   
+
+
+      <div className="App-Content">
+        <div className='Year-Title'>
+          Academic Year 22-23
+          <hr/>
+        </div>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM2101 Human Computer Interaction
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Sunbul Ahmad
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM2207 Introduction to the Theory of Computation
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Richard Booth, Aric Fowler
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM2208 Scientific Computing
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Frank Langbein, Paul Rosin
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM2305 Group Project
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM2306 Communication Networks
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Charith Perera
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM2307 Object Orientation, Algorithms and Data Structures
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Bailin Deng, Andrew Jones, Aisha Gul, David Humphreys, Jonathan Gorad
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM2102 Database Systems
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM2104 Computational Mathematics
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Paul Rosin, Stefano Zappala, Aric Fowler
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM2106 Enhancing your Employability
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
+
+
+      <div className="App-Content">
+        <div className='Year-Title'>
+          Academic Year 21-22
+          <hr/>
+        </div>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              Cm1205 Architecture and operating Systems
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Yipeng Qin
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM1208 Maths for Computer Science
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Yukun Lai, Aric Fowler, Yingying Wu
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM1210 Object Oriented Java Programming
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Matt Morgan, Neetesh Saxana, Surya Thottam Valappil
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM1102 Web Applications
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Martin Caminada, Crispin Cooper, Daniela Tsaneva
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM1301 Principles, Tools and Techniques for Secure Software Engineering
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM1101 Computational Thinking
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Kirill Sidorov, Jing Wu
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card style={{width: "30%", float: "left", marginLeft: "15px", marginBottom: "15px", textAlign: "left"}}>
+          <CardActionArea>
+            <CardContent style={{backgroundColor: "#7D7D7D", height: "50px", fontSize: "16px"}}>
+              CM1103 Problem Solving with Python
+            </CardContent>
+            <CardContent style={{color: "#FFFFFF", backgroundColor: "#A80000", height: "25px"}}>
+              Stuart Allen, Jing Wu
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
+
+
     </div>
   );
   
